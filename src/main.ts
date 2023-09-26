53330c20777d4fe53a20f90ea044646a870e81dc
Matheus de Lima <eclicknoplay@gmail.com>
2023-09-25 21:48:01 -0400

    Creating Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+        .setTitle('Documentos Unigran 1.1.1')
+        .setDescription('API envio e consulta documentos/diplomas')
+        .setVersion('1.05')
+        .addBearerAuth(
+            { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+            'access-token',
+        )
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3001, () => {
+        console.log(`Server listening at port 3001`);
+    });
 }
 bootstrap();
